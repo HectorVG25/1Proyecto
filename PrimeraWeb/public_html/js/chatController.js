a78f722bc3d39b476747007c07ad4488cf248744
Hector <hector.villamediana@educa.madrid.org>
2026-09-23 12:31:00 +0200

    Modificados chat.html y chatController.js

--- PrimeraWeb/public_html/js/chatController.js
+++ PrimeraWeb/public_html/js/chatController.js
@@ -2,13 +2,21 @@
  * @author Hector Villamediana
  */
 
-
-
 /**
- * 
- * @param {type} mensaje
- * @returns {undefined}
+ * Esta funcion envia los mensajes 
+ * @param {type} mensaje Los mensajes a enviar
+ * @returns {undefined} Undefined 
  */
 function enviarMensaje(mensaje){
-    
+    // Poniendo un <textarea> el usuario visualiza el mensaje a enviar.
+    //Tambien se definira q por defecto este escrito "Escriba Aqui".
+    let textoMensaje=document.getElementById('enviarBoton').value;
+    //Lo añadimos a la coleccion de mensajes
+    mensajes.add(new Mensaje(textoMensaje,new Date));
+
 }
+
+//Asoscio la funcion de enviar mensaje como manejadora del evento click del
+//elemento enviarBoton
+document.getElementById("enviarBoton").addEventListener('click',enviarMensaje);
+
--- PrimeraWeb/public_html/js/mensaje.js
+++ PrimeraWeb/public_html/js/mensaje.js
@@ -4,5 +4,5 @@
 
 class Mensaje {
 
-    fechaHora;
+    dateTime;
 }

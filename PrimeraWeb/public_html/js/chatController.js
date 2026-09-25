c46b7b65f653bb686bed5472041b4c395df3e1dd
Francisco <fran.solorzano126@gmail.com>
2026-09-25 13:07:07 +0200

    Actualiazado

--- PrimeraWeb/public_html/js/chatController.js
+++ PrimeraWeb/public_html/js/chatController.js
@@ -8,24 +8,29 @@ var mensajes = new Array();
 /**
  * Esta funcion muestra todos los mensajes del parametro 
  * en una pagina en forma de texto dentro de un contenedor DIV
- * @param {type} mensajes La coleecion de mensajes a mostrar
+ * @param {type}
  * @returns {undefined}
  */
-function actualizarMensajes(mensajes){
+function actualizarMensajes() {
     //ordenar mensaje por fecha de mensaje de forma que vaya de mas reciente a 
     //mas antiguo
-    
-    
+    mensajes.reverse();
     //Recorrer la coleccion de mensajes
-    
-    
+    for (let i = 0; i < mensajes.length; i++) {
+        //En cada iteracion añadimos al elemento <DIV> contenido consistente en 
+        //texto del mensaje
+        //dentro de un elemento de lista desordenada <UL> y <LI>
+        document.getElementById("msgList").textContent= "<li>" + mensajes[i].text + "</li>";
+        //Actualizamos la lista de mensajes
+        
+    }
     //En cada iteracion añadimos al elemento <DIV> contenido consistente en 
     //texto del mensaje
     //dentro de un elemento de lista desordenada <UL> y <LI>
-    
-    
+
+
 }
 
 //Asocio a la funcion actualizarMensajes como manejadora del evento de carga del
 //DOM de la pagina 
-document.addEventListener('DCMContentLoaded', actualizarMensajes(mensajes));
+document.addEventListener('DCMContentLoaded', actualizarMensajes);
